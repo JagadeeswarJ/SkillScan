a9ef4ea0e4413c92a23d49aaef6629f947322e9e
Jagadeeswar_J <115331104+Jagadeeswar072@users.noreply.github.com>
2025-08-10 10:54:11 +0530

    BE testing

---
+++ app/api/server/route.ts
@@ -0,0 +1,11 @@
+export async function GET() {
+    return new Response(
+        JSON.stringify({ message: "Hello from Next.js API (TypeScript)!" }),
+        {
+            status: 200,
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }
+    );
+}
